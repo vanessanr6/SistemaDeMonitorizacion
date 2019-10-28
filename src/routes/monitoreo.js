b81db84ea3b6fea1058a117ac07f3fb8ba7fa334
dennisrmz <orellanadennis12@gmail.com>
2019-10-27 23:22:09 -0600

    Primer avance pantallas

--- src/routes/monitoreo.js
+++ src/routes/monitoreo.js
@@ -1,4 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+var Chart = require('chart.js');
+
 module.exports = router;
+
+// *******************Rutas Para Muestras de Datos****************
+router.get('/monitoreo_principal', async (req, res) => {
+    res.render('monitoreo/principal');
+    });
+
+router.get('/grafica', async (req, res) => {
+    res.render('monitoreo/grafica');
+    });
